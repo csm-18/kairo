989330a97fe988957c4caa8a402e0808f95b5371
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-09-08 22:58:12 +0530

    add cli::run()

---
+++ src/cli/mod.rs
@@ -0,0 +1,4 @@
+// parse and execute command-line arguments passed to the compiler
+pub fn run(args: Vec<String>) {
+    dbg!(args);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+mod cli;
+
+// stand-alone cli compiler entry-point
 fn main() {
+    // command-line arguments
     let mut args: Vec<String> = std::env::args().skip(1).collect();
 
-    dbg!(args);
+    // start the compiler cli with command-line arguments
+    cli::run(args);
 }
