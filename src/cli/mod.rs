fa4805e0359771577056d5fdf89534e44ba5677e
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-09-09 11:44:54 +0530

    build vs normal mode check

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,4 +1,10 @@
 // parse and execute command-line arguments passed to the compiler
 pub fn run(args: Vec<String>) {
-    dbg!(args);
+    if !args.is_empty() && (args[0] == "--build" || args[0] == "-b") {
+        // build mode
+        println!("Build mode is not implemented yet!");
+    } else {
+        // normal mode
+        dbg!(args);
+    }
 }
