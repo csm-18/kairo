7df041de4cf0f8b076f50f70fe6655101ecc21ef
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-09-09 22:45:28 +0530

    fix bug in build mode check

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -5,7 +5,9 @@ use arg_parser::parse_args;
 
 // parse and execute command-line arguments passed to the compiler
 pub fn run(args: &mut Vec<String>) {
-    if !args.is_empty() && (args[0] == "--build" || args[0] == "-b") {
+    if !args.is_empty()
+        && (args[0] == "--build" || args[0] == "-build" || args[0] == "-b" || args[0] == "--b")
+    {
         // build mode
         println!("Build mode is not implemented yet!");
     } else {
